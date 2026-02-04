c6c6cd8227d053903fd332c229c5dd1014fe727b
sivanagi reddy akkili <sivanagireddyakkili@gmail.com>
2026-02-04 19:24:03 +0530

    updated the file search_in_Strings.java

--- src/Search_in_Strings.java
+++ src/Search_in_Strings.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
 public class Search_in_Strings {
+    public static void main(String[] args) {
+        String name="eswar reddy akkili";
+        char target= 'w';
+        boolean ans=linearSearch(name,target);
+        System.out.println(ans);
+
+
+
+    }
+    static boolean linearSearch(String str,char target){
+        if(str.length()==0){
+            return false;
+
+        }
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)==target){
+
+                return true;
+            }
+
+        }
+        return false;
+    }
 }
