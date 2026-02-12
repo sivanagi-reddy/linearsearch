3dc9763bf59268d922411004c753f746791691ef
sivanagi reddy akkili <sivanagireddyakkili@gmail.com>
2026-02-13 00:32:26 +0530

    solved the richest customer wealth

--- src/Maxwealth.java
+++ src/Maxwealth.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
-
 public class Maxwealth {
+
+    //https://leetcode.com/problems/richest-customer-wealth/description/?envType=problem-list-v2&envId=array
+    static void main() {
+
+    }
+    public int max(int[][]accounts){
+        //person=rol
+        //account =col
+        int max_wealth =Integer.MIN_VALUE;
+        for (int[] ints : accounts) { //when you start a new col, take a new sum for that row
+
+            int sum = 0;
+            for (int anInt : ints) {
+                sum += anInt;
+                if (sum > max_wealth) {
+                    max_wealth = sum;
+                }
+
+
+            }
+
+        }
+        return max_wealth;
+    }
+
 }
